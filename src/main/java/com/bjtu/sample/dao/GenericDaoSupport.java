5b2c47ef83d4c14e69505b6cbb7c08b3e1d7c43f
Riko Huang <ric-ko-92@hotmail.com>
2013-07-07 17:07:56 +0800

    Added Component annotation

--- src/main/java/com/bjtu/sample/dao/GenericDaoSupport.java
+++ src/main/java/com/bjtu/sample/dao/GenericDaoSupport.java
@@ -11,12 +11,15 @@ import org.hibernate.Criteria;
 import org.hibernate.Query;
 import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
 /**
  * @author Downpour
  */
+@Component
 public class GenericDaoSupport {
 	
+	@Autowired
 	private SessionFactory sessionFactory;
 
 	/**
