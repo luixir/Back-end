1fe17b59cfa2f992c88fea2a8965b8bbf2b3b47f
Riko Huang <ric-ko-92@hotmail.com>
2013-07-13 11:36:12 +0800

    Add create method

--- src/main/java/com/bjtu/web/controller/CategoryController.java
+++ src/main/java/com/bjtu/web/controller/CategoryController.java
@@ -5,13 +5,14 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.bjtu.entity.Category;
 import com.bjtu.service.CategoryService;
 
 @Controller
-@RequestMapping("/category") //namespace
+@RequestMapping("/categories") //namespace
 public class CategoryController {
 	
 	@Autowired
@@ -22,7 +23,7 @@ public class CategoryController {
 	 * 
 	 * @return
 	 */
-	@RequestMapping("categories") //action
+	@RequestMapping("") //action
 	public ModelAndView list() {
 		List<Category> categories = categoryService.listAllCategories();
 		ModelAndView modelAndView = new ModelAndView("category-list");
@@ -31,4 +32,33 @@ public class CategoryController {
 		return modelAndView;
 	}
 	
+	/**
+	 * Prepare to create a category
+	 * 
+	 * @return
+	 */
+	@RequestMapping("/create")
+	public ModelAndView create() {
+		
+		List<Category> rootCategories = categoryService.listParentCategories();
+		ModelAndView modelAndView = new ModelAndView("category-create");
+		modelAndView.addObject("rootCategories", rootCategories);
+		
+		return modelAndView;
+	}
+	
+	/**
+	 * Create a category
+	 * 
+	 * @param category
+	 * @return
+	 */
+	@RequestMapping( value = "/create", method = RequestMethod.POST)
+	public ModelAndView onCreate(Category category) {
+		categoryService.addCategory(category);
+		
+		ModelAndView modelAndView = new ModelAndView("redirect:/categories");
+		
+		return modelAndView;
+	}
 }
