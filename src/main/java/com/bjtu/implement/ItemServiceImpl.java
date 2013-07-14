b40ecf907565689252a0c887593e1e3ebdc5c48b
Riko Huang <ric-ko-92@hotmail.com>
2013-07-14 10:34:13 +0800

    Added new item service

--- src/main/java/com/bjtu/implement/ItemServiceImpl.java
+++ src/main/java/com/bjtu/implement/ItemServiceImpl.java
@@ -22,6 +22,11 @@ public class ItemServiceImpl implements ItemService{
 	@Autowired
 	private GenericDaoSupport genericDaoSupport;
 	
+	public List<Item> listAllItem() {
+		String querySentence = "FROM Item item";
+		return genericDaoSupport.searchForList(querySentence, new HashMap<String, Object>());
+	}
+	
 	public List<Item> listEndingItems() {
 		String querySentence = "FROM Item item WHERE item.endDate = :today";
 		Map<String, Object> parameters = new HashMap<String, Object>();
@@ -33,4 +38,14 @@ public class ItemServiceImpl implements ItemService{
 		genericDaoSupport.save(item);
 	}
 
+	public void deleteItem(Item item) {
+		genericDaoSupport.delete(item);
+	}
+
+	public List<Item> getItemByCategory(String name) {
+		String querySentence = "FROM Item item WHERE item.category = :name";
+		Map<String, Object> parameters = new HashMap<String, Object>();
+		parameters.put("name", name);
+		return genericDaoSupport.searchForList(querySentence, parameters);
+	}
 }
--- src/main/java/com/bjtu/service/ItemService.java
+++ src/main/java/com/bjtu/service/ItemService.java
@@ -2,6 +2,7 @@ package com.bjtu.service;
 
 import java.util.List;
 
+import com.bjtu.entity.Category;
 import com.bjtu.entity.Item;
 
 /**
@@ -10,8 +11,14 @@ import com.bjtu.entity.Item;
  */
 public interface ItemService {
 	
+	public List<Item> listAllItem();
+	
 	public List<Item> listEndingItems();
 	
 	public void addItem(Item item);
+	
+	public void deleteItem(Item item);
+	
+	public List<Item> getItemByCategory(String name);
 	
 }
